8881d297c906b00b2f8912e0b37afa7b4836f163
Aaron Wells <wellsaar@iu.edu>
2022-04-18 14:43:07 -0400

    setup inventory for ansible

--- output.tf
+++ output.tf
@@ -3,21 +3,15 @@
 ################
 
 
-output "floating_ip_ubuntu20_leader" {
-  value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_leader.*.address
-  description = "Public IP for Ubuntu 20 leader"
-}
-
-output "floating_ip_ubuntu20_follower" {
-  value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_follower.*.address
-  description = "Public IP for Ubuntu 20 followers"
+output "floating_ip_ubuntu20" {
+  value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.address
+  description = "Public IP for Ubuntu 20"
 }
 
 resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/inventory.tftpl",
   {
-    ansible_leader_ip    = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_leader.*.address,
-    ansible_follower_ip  = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_follower.*.address,
+    ubuntu20    = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.*.address,
   }
   )
   filename  = "ansible/inventory.ini"
