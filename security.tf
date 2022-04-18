09b95f27a4587772a75a558aab81f6d2b83e3c83
Aaron Wells <wellsaar@iu.edu>
2022-04-18 14:01:26 -0400

    cloned existing terraform repo

---
+++ security.tf
@@ -0,0 +1,23 @@
+################
+#Security section
+################
+#creating security group
+resource "openstack_compute_secgroup_v2" "terraform_ssh_ping_centos" {
+  name = "terraform_ssh_ping"
+  description = "Security group with SSH and PING open to 0.0.0.0/0"
+
+  #ssh rule
+  rule{
+    ip_protocol = "tcp"
+    from_port  =  "22"
+    to_port    =  "22"
+    cidr       = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = -1
+    to_port     = -1
+    ip_protocol = "icmp"
+    cidr        = "0.0.0.0/0"
+  }
+
+}
