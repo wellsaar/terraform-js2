3c08e21622a01628152352641c2c181a257ab497
Aaron Wells <wellsaar@iu.edu>
2022-04-27 14:53:42 -0400

    added remote provisioners for copying over ansible

--- Ubuntu20.tf
+++ Ubuntu20.tf
@@ -21,23 +21,47 @@ resource "openstack_compute_instance_v2" "Ubuntu20" {
   flavor_id   = 3
   # you'll need to set this to your public key name on jetstream
   key_pair  = "wellsaar"
-  security_groups   = ["terraform_ssh_ping", "default"]
+  security_groups   = ["terraform_ssh_ping_http_https", "default"]
   metadata = {
     terraform_controlled = "yes"
   }
   network {
     name = "terraform_network"
   }
+
   depends_on = [openstack_networking_network_v2.terraform_network]
-
 }
 
-resource "openstack_networking_floatingip_v2" "terraform_floatip_ubuntu20" {
-  pool = "public"
-}
+resource "null_resource" "ansible_provisioners" {
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt install python3 ansible -y",
+    ]
+      connection {
+        type = "ssh"
+        host = "${openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.address}"
+        user = "ubuntu"
+      }
+  }
+  provisioner "file" {
+    source = "ansible"
+    destination = "ansible"
+  }
+      connection {
+        type = "ssh"
+        host = "${openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.address}"
+        user = "ubuntu"
+      }
+  provisioner "remote-exec" {
+    inline = [
+      "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i ansible/inventory.ini ansible/main.yml ",
+    ]
+      connection {
+        type = "ssh"
+        host = "${openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.address}"
+        user = "ubuntu"
+      }
+    }
 
-# assigning floating ip from public pool to Ubuntu20 VM
-resource "openstack_compute_floatingip_associate_v2" "Ubuntu20_float" {
-  floating_ip = "${openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.address}"
-  instance_id = "${openstack_compute_instance_v2.Ubuntu20.id}"
+  depends_on = [openstack_compute_floatingip_associate_v2.Ubuntu20_float]
 }
