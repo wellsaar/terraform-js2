c6c7b453eb8fbcf3fb6b1fed0d94d5d59eb4b92c
Aaron Wells <wellsaar@iu.edu>
2022-07-07 14:36:45 -0400

    remvoed image id since we call image by name now

--- ubuntu_nginx_mariadb/variables.tf
+++ ubuntu_nginx_mariadb/variables.tf
@@ -1,9 +1,3 @@
-variable "image_id" {
-  #Ubuntu 20 on JS2
-  default = "a49148ce-f119-4180-b913-009546052f18"
-  description = "The id of the image from Jetstream 2"
-}
-
 variable "flavor_id" {
   default = "3"
   description = "Flavor of VM on Jetstream 2"
