733d54c3f8ed9aca10c4209cb838bef01166755c
Aaron Wells <wellsaar@iu.edu>
2022-11-30 09:54:46 -0500

    reformatted and changed version to ubuntu22

--- ubuntu_nginx_r_shiny/Ubuntu20.tf
+++ ubuntu_nginx_r_shiny/Ubuntu20.tf
@@ -3,31 +3,31 @@
 ################
 # creating leader
 
-resource "openstack_networking_floatingip_v2" "terraform_floatip_ubuntu20" {
+resource "openstack_networking_floatingip_v2" "terraform_floatip_ubuntu22" {
   pool = "public"
 }
 
 # assigning floating ip from public pool to Ubuntu20 VM
-resource "openstack_compute_floatingip_associate_v2" "Ubuntu20_float" {
-  floating_ip = "${openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.address}"
-  instance_id = "${openstack_compute_instance_v2.Ubuntu20.id}"
+resource "openstack_compute_floatingip_associate_v2" "Ubuntu22_float" {
+  floating_ip           = openstack_networking_floatingip_v2.terraform_floatip_ubuntu22.address
+  instance_id           = openstack_compute_instance_v2.Ubuntu22.id
   wait_until_associated = true
 }
 
-resource "openstack_compute_instance_v2" "Ubuntu20" {
-  name = "shiny"
-  image_name = "Featured-Ubuntu20"
-  flavor_id = var.flavor_id
+resource "openstack_compute_instance_v2" "Ubuntu22" {
+  name       = "shiny"
+  image_name = "Featured-Ubuntu22"
+  flavor_id  = var.flavor_id
   # you'll need to set this to your public key name on jetstream
-  key_pair  = var.key_pair
-  security_groups   = ["terraform_ssh_ping_http_https_r_shiny", "default"]
+  key_pair        = var.key_pair
+  security_groups = ["SSH and ICMP Ingress", "default", "HTTP/HTTPS Ingress"]
   metadata = {
     terraform_controlled = "yes"
-    terraform_role = "r_shiny"
+    terraform_role       = "r_shiny"
   }
-   network {
-     name = "auto_allocated_network"
-   }
+  network {
+    name = "auto_allocated_network"
+  }
 }
 
 resource "null_resource" "ansible_provisioners" {
@@ -39,31 +39,31 @@ resource "null_resource" "ansible_provisioners" {
       "sudo apt install python3 ansible -y",
       "rm -rf ~/ansible"
     ]
-      connection {
-        type = "ssh"
-        host = "${openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.address}"
-        user = "ubuntu"
-      }
+    connection {
+      type = "ssh"
+      host = openstack_networking_floatingip_v2.terraform_floatip_ubuntu22.address
+      user = "ubuntu"
+    }
   }
   provisioner "file" {
-    source = "ansible"
+    source      = "ansible"
     destination = "ansible"
   }
-      connection {
-        type = "ssh"
-        host = "${openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.address}"
-        user = "ubuntu"
-      }
+  connection {
+    type = "ssh"
+    host = openstack_networking_floatingip_v2.terraform_floatip_ubuntu22.address
+    user = "ubuntu"
+  }
   provisioner "remote-exec" {
     inline = [
       "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i ansible/inventory.ini ansible/main.yml ",
     ]
-      connection {
-        type = "ssh"
-        host = "${openstack_networking_floatingip_v2.terraform_floatip_ubuntu20.address}"
-        user = "ubuntu"
-      }
+    connection {
+      type = "ssh"
+      host = openstack_networking_floatingip_v2.terraform_floatip_ubuntu22.address
+      user = "ubuntu"
     }
+  }
 
-  depends_on = [openstack_compute_floatingip_associate_v2.Ubuntu20_float]
+  depends_on = [openstack_compute_floatingip_associate_v2.Ubuntu22_float]
 }
